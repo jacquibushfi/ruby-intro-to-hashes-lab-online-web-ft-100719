b49a5ab4f7c3a39f1f684d1a9a8b73e7a7150346
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-10 19:27:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,11 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads =>
-	   :pieces => {
+	   {
+	     :pieces => {
 	     4 },
-	   :names => {}
+	     :names => {}
+	   }
 	}
 
 end
